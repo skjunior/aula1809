e3ce5a9edf6c4b18584a7b347777d1d3854820f6
skjunior <142546585+skjunior@users.noreply.github.com>
2023-09-18 20:52:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+/*let valor = 0
+for(let i = 0; i < 5; i++) {
+valor += i
+}
+console.log(valor)
+// o resultado no log é 10 pq o resultado da primeira operação é vdd
+//o prpograma vai ser executado ate q entre o conchetes tenha um resultado falso
+
+
+// resposta 2
+//a) vai ser impresso todos os numeros maiores q o 18.
+//b) para ver os itens da lista 
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+console.log(numero);
+}
+*/
+//resposta 3
+//vão ser irpressor 4 linhas com o numero de asteriscos equivalente ao de linhas.
+
+// exercicio de codigo numero 1
+
+// resposta 2
+//a) e b)
+const arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
+
+for (const elemento of arrayOriginal) {
+    console.log(elemento / 10);
+}
+let par = [];
